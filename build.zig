764ef8893d547a52653f3033ffefd07eb9d7f598
Tristan Pemble <tpemble@gmail.com>
2025-07-25 14:24:33 -0700

    add github workflows

--- build.zig
+++ build.zig
@@ -1,29 +1,39 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    // Options
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.addModule("restruct", .{
-        .root_source_file = b.path("src/root.zig"),
+    // Module
+    const mod = b.addModule("restruct", .{
+        .root_source_file = b.path("src/restruct.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    // Library
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "restruct",
-        .root_module = lib_mod,
+        .root_module = mod,
     });
-
     b.installArtifact(lib);
 
+    // Test
     const unit_tests = b.addTest(.{
-        .root_module = lib_mod,
+        .root_module = mod,
     });
-
     const run_unit_tests = b.addRunArtifact(unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
+
+    // Docs
+    const docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    const docs_step = b.step("docs", "Install docs into zig-out/docs");
+    docs_step.dependOn(&docs.step);
 }
